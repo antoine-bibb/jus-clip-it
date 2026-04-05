c707a2456800c768a63ba5bc9ccc6d164e957b02
antoine-bibb <ajbibb0114@gmail.com>
2026-04-05 15:09:58 -0400

    feat: Update CORS middleware to allow additional origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ from app.core.db import Base, engine
 app = FastAPI(title=settings.app_name)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000', 'http://127.0.0.1:3000'],
+    allow_origins=['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
